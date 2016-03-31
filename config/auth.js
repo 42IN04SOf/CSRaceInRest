245f89c8e6355d50bb5370450e98e2184420569c
Marthijn Verhoeven <mmamhver@student.avans.nl>
2016-03-31 12:12:38 +0200

    callback

--- config/auth.js
+++ config/auth.js
@@ -4,19 +4,19 @@ module.exports = {
     'facebookAuth' : {
         'clientID'      : 'your-secret-clientID-here', // your App ID
         'clientSecret'  : 'your-client-secret-here', // your App Secret
-        'callbackURL'   : 'http://localhost:8080/auth/facebook/callback'
+        'callbackURL'   : 'https://rrfm.herokuapp.com/auth/facebook/callback'
     },
 
     'twitterAuth' : {
         'consumerKey'       : 'your-consumer-key-here',
         'consumerSecret'    : 'your-client-secret-here',
-        'callbackURL'       : 'http://localhost:8080/auth/twitter/callback'
+        'callbackURL'       : 'https://rrfm.herokuapp.com/auth/twitter/callback'
     },
 
     'googleAuth' : {
         'clientID'      : '74184454365-t803u4uvb5l14om7a2qmomg5i4s74irl.apps.googleusercontent.com',
         'clientSecret'  : 'bP-sHknCcfIuU6j-PgKMpFNK',
-        'callbackURL'   : 'http://localhost:8080/auth/google/callback'
+        'callbackURL'   : 'https://rrfm.herokuapp.com/auth/google/callback'
     }
 
 };
